f061276522276c5f91ceef74f102bff660ee9c0e
elliotconner <elliot.conner@icloud.com>
2025-11-20 18:35:54 -0500

    Initial Commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,7 +7,5 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index'),
-    # Page that shows all topics.
-    path('topics/', views.topics, name='topics'),
+    path('', views.index, name='index')
 ]
