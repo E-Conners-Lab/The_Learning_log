a2efce1faf0dcd6245fed9b2b4d09bdcb51426b9
elliotconner <elliot.conner@icloud.com>
2025-11-21 00:45:57 -0500

    Created individual topic pages

--- meal_planner/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    """The home page for Meal Planner"""
-    return render(request, 'meal_planner/index.html')
