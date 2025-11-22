69d412218a906f03af08b079dbb494a402858af4
elliot <elliot.conner@icloud.com>
2025-11-21 22:33:31 -0500

    Ready for deplyment to Platform.sh

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -137,3 +137,29 @@ LOGOUT_URL = 'accounts:login'
 FORM_RENDERER = 'django.forms.renderers.DjangoDivFormRenderer'
 
 
+# Platform.sh settings.,
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / 'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials('database')
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': db_settings['path'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
+                'HOST': db_settings['host'],
+                'PORT': db_settings['port'],
+            },
+        }
+
+
